c1994d4825ce16b4f317207d5e77b96b9abcc8a0
Christine Yu <christine.tc.yu@gmail.com>
2016-03-01 22:36:42 -0500

    refactor

--- bin.js
+++ bin.js
@@ -1,33 +1,40 @@
 #! /usr/bin/env node
 var shell = require("shelljs");
 var fs = require('fs');
-var resolvePath  = require('path').resolve;
-var joinPath  = require('path').join;
 
 var wantedRemote = process.argv[2];
 
 var walkDirs = (cwd) => {
-  var paths = fs.readdirSync(cwd).filter(p => p.charAt(0) !== '.');
-  paths.forEach(path => {
-    var fullPath = cwd + '/' + path;
-    try {
-      if(fs.statSync(fullPath).isDirectory()) {
-        if (fs.existsSync(fullPath + '/' + '.git')) {
-          shell.exec("git config --get remote.origin.url",
-                    { cwd: fullPath, silent: true },
-                    (code, stdout, stderr) => {
-                      if (wantedRemote === stdout.replace(/^\s+|\s+$/g, '')) {
-                        console.log(fullPath);
-                      }
-                    });
-        } else {
-          walkDirs(fullPath);
-        }
-      }
-    } catch(e) {
-      console.log('error checking' + fullPath);
-    }
+  var paths = fs.readdirSync(cwd)
+                .filter(p => {
+                  try {
+                    return p.charAt(0) !== '.' && fs.statSync(cwd + '/' + p).isDirectory();
+                  } catch(e) {
+                    console.log(e);
+                  }
+                })
+                .reduce((acc, p) => {
+                  var fullPath = cwd + '/' + p;
+                  if (fs.existsSync(fullPath + '/' + '.git')) {
+                    acc.gitDirs = acc.gitDirs.concat(p);
+                  } else {
+                    acc.others = acc.others.concat(p);
+                  }
+                  return acc;
+                }, {gitDirs: [], others: []});
+
+  paths.gitDirs.forEach(p => {
+    var fullPath = cwd + '/' + p;
+    shell.exec("git config --get remote.origin.url",
+               { cwd: fullPath, silent: true },
+               (code, stdout, stderr) => {
+                  if (wantedRemote === stdout.replace(/^\s+|\s+$/g, '')) {
+                    console.log(fullPath);
+                  }
+                });
   });
+
+  paths.others.forEach(p => { walkDirs(cwd + '/' + p) });
 }
 
 walkDirs(process.cwd());
